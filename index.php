ac64ee310879bac33a497c131fff0d1c505f441b
gdjuarez <33164842+gdjuarez@users.noreply.github.com>
2023-02-08 12:50:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
-echo Date('Y');
+//echo Date('Y');
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+</head>
+<body>
+    
+</body>
+</html>
