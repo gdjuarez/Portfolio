fd4cfe4db108844e1dc744f81c33430e63f3c7b1
gdjuarez <33164842+gdjuarez@users.noreply.github.com>
2023-02-16 19:50:20 -0300

    imagenes

--- index.php
+++ index.php
@@ -9,9 +9,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!--fontawesome icons-->
-   <!-- Font Awesome -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"  rel="stylesheet"
-/>
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- jquery  -->
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"
         integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
@@ -19,11 +18,9 @@
     <title>Portfolio</title>
     <!--  CSS personalizado -->
     <link href="assets/css/style.css" rel="stylesheet">
-     <!-- js personalizado -->
-     <script src="assets/js/escribe_texto.js"></script>
-   
+    <!-- js personalizado -->
+    <script src="assets/js/escribe_texto.js"></script>
 
-     
 </head>
 
 <body>
@@ -40,114 +37,137 @@
         </nav>
 
     </header>
+    <div class="main">
+        <!-- INICIO -->
+        <section id="inicio" class="d-flex flex-column justify-content-center ">
+            <div class="container">
+                <h1 class='nombre mb-4'>Gustavo Juarez</h1>
+                <p class='actividad' id='actividad'><span>-</span></p>
 
-    <!-- INICIO -->
-    <section id="inicio" class="d-flex flex-column justify-content-center ">
-        <div class="container">
-            <h1 class='nombre mb-4'>Gustavo Juarez</h1>
-            <p class='actividad' id='actividad'><span>-</span></p>
-         
-            <div class="red_social_instagram">
-                <a href="#" class="instagram">
-                <i class="fab fa-instagram"></i>
-                </a>
-            </div>
-            <div class="red_social_ln">
-                <a href="#" class="linkedin">
-                <i class="fab fa-linkedin"></i>
-                </a>
-            </div>
-        </div>
-    </section>
-
-    <!-- TRABAJOS -->
-    <section id="trabajos" class="trabajos p-4">
-        <div class="container">
-            <h2 class='text-white'>Trabajos</h2>
-           <!-- CARRUSELL -->
-           
-           <!--  -->
-            
-        </div>
-    </section>
-
-    <!--SOBRE MI-->
-    <section id="sobremi" class="sobremi ">
-        <div class="container">
-            <div class="row">
-            <h2>Sobre mi</h2>
-            <div class="col-md-2"></div>
-            <div class="col-md-4">
-                <div class="parrafo p-5">
-                    <p class="text-white">Soy analista de sistemas, desarrollo
-                        sistemas web a medida para Comercios, Pymes y Profesionales.</p>
-                    <p class="text-white">Tengo pasion por la programación y la
-                        creación de métodos que rindan soluciones prácticas.
-                        Mencantan los desafios y el aprendizaje continuo.</p>
-                    <p class="text-white">En mis primeros años desarrolle aplicaciones
-                        y sistemas en Visual Basic .Net,
-                        luego comence a realizar Apps moviles con Android Studio y continue con la programacion
-                        Web con PHP y Mysql para el Back-End
-                        y Bootstrap, Javascript (JQuery) y Css para el Front-End</p>
+                <div class="red_social_instagram">
+                    <a href="#" class="instagram">
+                        <i class="fab fa-instagram"></i>
+                    </a>
                 </div>
-                <div class="col-md-2"></div>
+                <div class="red_social_ln">
+                    <a href="#" class="linkedin">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </div>
             </div>
+        </section>
+
+        <!-- TRABAJOS -->
+        <section id="trabajos" class="trabajos p-4">
+            <div class="container">
+                <h2 class='text-white'>Trabajos</h2>
+                <div class="grid">
+                    <article class="article">
+                        <a href="#"></a>
+                        <figure class="figure">
+                            <img src="assets/img/HEC.JPG" alt="" srcset="">
+                        </figure>
+                    </article>
+                    <article class="article">
+                        <a href="#"></a>
+                        <figure class="figure">
+                            <img src="assets/img/maderas_mercedes.JPG" alt="" srcset="">
+                        </figure>
+                    </article>
+                    <article class="article">
+                        <a href="#"></a>
+                        <figure class="figure">
+                            <img src="assets/img/prestamos.JPG" alt="" srcset="">
+                        </figure>
+                    </article>
+                    <article class="article">
+                        <a href="#"></a>
+                        <figure class="figure">
+                            <img src="assets/img/mitho.JPG" alt="" srcset="">
+                        </figure>
+                    </article>
+                   
+
+                </div>
+
             </div>
-        </div>
-    </section>
+        </section>
 
-    <!-- CONTACTO -->
-
-    <section id="contacto" class="contacto bg-light">
-        <div class="container text-center">
-            <div class="section-title">
-                <h2 class='p-4'>Contacto</h2>
-            </div>
-            <div class="row mt-1">
-
-                <div class="col-lg-4">
-                    <div class="email">
-                        <i class="bi bi-envelope"></i>
-                        <h4>Email:</h4>
-                        <p>dev@gdjuarez.site</p>
+        <!--SOBRE MI-->
+        <section id="sobremi" class="sobremi ">
+            <div class="container">
+                <div class="row">
+                    <h2>Sobre mi</h2>
+                    <div class="col-md-2"></div>
+                    <div class="col-md-4">
+                        <div class="parrafo p-5">
+                            <p class="text-white">Soy analista de sistemas, desarrollo
+                                sistemas web a medida para Comercios, Pymes y Profesionales.</p>
+                            <p class="text-white">Tengo pasion por la programación y la
+                                creación de métodos que rindan soluciones prácticas.
+                                Mencantan los desafios y el aprendizaje continuo.</p>
+                            <p class="text-white">En mis primeros años desarrolle aplicaciones
+                                y sistemas en Visual Basic .Net,
+                                luego comence a realizar Apps moviles con Android Studio y continue con la programacion
+                                Web con PHP y Mysql para el Back-End
+                                y Bootstrap, Javascript (JQuery) y Css para el Front-End</p>
+                        </div>
+                        <div class="col-md-2"></div>
                     </div>
                 </div>
-
-                <div class="col-lg-6 ">
-                    <form action="forms/contact.php" method="post" role="form" class="php-email-form">
-                        <div class="form-row">
-                            <div class="row">
-                                <div class="col-md-6 form-group">
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="nombre"
-                                        required>
-                                </div>
-                                <div class="col-md-6 form-group mt-3 mt-md-0">
-                                    <input type="email" class="form-control" name="email" id="email"
-                                        placeholder="correo" required>
-                                </div>
-                            </div>
-                            <div class="form-group mt-3">
-                                <input type="text" class="form-control" name="subject" id="subject" placeholder="Asunto"
-                                    required>
-                            </div>
-                            <div class="form-group mt-3">
-                                <textarea class="form-control" name="message" rows="5" placeholder="Mensaje"
-                                    required></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary text-center mt-2">Enviar mensaje</button>
-                    </form>
-                </div>
-
-                <div class="col-2">
-
-                </div>
-
             </div>
-        </div>
-    </section>
+        </section>
 
+        <!-- CONTACTO -->
 
+        <section id="contacto" class="contacto bg-light">
+            <div class="container text-center">
+                <div class="section-title">
+                    <h2 class='p-4'>Contacto</h2>
+                </div>
+                <div class="row mt-1">
 
+                    <div class="col-lg-4">
+                        <div class="email">
+                            <i class="bi bi-envelope"></i>
+                            <h4>Email:</h4>
+                            <p>dev@gdjuarez.site</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 ">
+                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                            <div class="form-row">
+                                <div class="row">
+                                    <div class="col-md-6 form-group">
+                                        <input type="text" name="name" class="form-control" id="name"
+                                            placeholder="nombre" required>
+                                    </div>
+                                    <div class="col-md-6 form-group mt-3 mt-md-0">
+                                        <input type="email" class="form-control" name="email" id="email"
+                                            placeholder="correo" required>
+                                    </div>
+                                </div>
+                                <div class="form-group mt-3">
+                                    <input type="text" class="form-control" name="subject" id="subject"
+                                        placeholder="Asunto" required>
+                                </div>
+                                <div class="form-group mt-3">
+                                    <textarea class="form-control" name="message" rows="5" placeholder="Mensaje"
+                                        required></textarea>
+                                </div>
+                                <button type="submit" class="btn btn-primary text-center mt-2">Enviar mensaje</button>
+                        </form>
+                    </div>
+
+                    <div class="col-2">
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </div>
 
     <footer id="footer">
         <div class="copyright bg-dark text-white text-center rounded">
